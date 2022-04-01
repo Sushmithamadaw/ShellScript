776bab2678137432657e07597360ea5364d03c2b
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-04-01 15:18:41 +0530

    calender

---
+++ 5b.sh
@@ -0,0 +1,9 @@
+date=`date +%d`
+date=`expr $date + 0`
+line=`cal | tr -d "[\137][\010]" | grep -wn $date | head -c 1`
+  if [ $date -gt 9 ]
+   then 
+   cal | tr -d "[\137][\010]" | sed "$line s/$date/**/"
+   else
+   cal | tr -d "[\137][\010]" | sed "$line s/$date/*/"
+ fi
