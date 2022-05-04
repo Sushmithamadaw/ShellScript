57c145350ff2fd4fb2cd48e38d44361823e87807
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-05-04 14:17:55 +0530

    this is 6a program

--- 6a.sh
+++ 6a.sh
@@ -1,10 +1,25 @@
-#6b. Write a shell script to list all the files in a directory whose filename is at least 10 characters.
-#(use expr command to check the length).
-for i in `ls`
-do 
-fl=`expr length $i`
-if [ $fl -ge 0 ]
-then 
-echo $i
-fi
+#6a. Write s a shell script to find a file/s that matches a pattern given as command line argument
+#in the home directory, display the contents of the file and copy the file into the directory
+#~/mydir.
+
+if [ $# -eq 1 ]
+then
+if [ -d ~/mydir ]
+then
+set -- `find ~ -name "$1"`
+for i in $*
+do
+cp $i ~/mydir 2>er
 done
+if [ $# -eq 0 ]
+then
+echo "No such file"
+else
+cat $i
+fi
+else
+echo "No such directory"
+fi
+else
+echo enter argument
+fi
