cf92dc4dbce6eaef3e31b59b7ff0225ac1be34d6
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-05-06 16:12:32 +0530

    updated

--- 9b.sh
+++ 9b.sh
@@ -2,29 +2,29 @@ echo -n "Ente the file name:"
 read fname
 if [ -f $fname ]
 then
-	n=10
+	n=40
 	while read line
 	do
-		l1=`expr length "line"`
-		if [ $l1 -lt $n ]
+		l=`expr length "$line"`
+		if [ $l -le $n ]
 		then
 			echo $line
 		else 
 			s=1
 			e=$n
-			echo "`echo $line | cut -c $s-$e`\\"
-			rcl=`expr $l1 -$e`
+			echo "`echo $line | cut -c $s-$e` \\"
+			rcl=`expr $l - $e`
 			while [ $rcl -ge $n ]
 			do
 				s=`expr $e + 1`
 				e=`expr $e + $n`
-				echo "`echo $line | cut -c $s-$e`\\"
-				rcl=`expr $l1 -$e`
+				echo "`echo $line | cut -c $s-$e` \\"
+				rcl=`expr $l - $e`
 			done
 			s=`expr $e + 1`
 			echo $line | cut -c $s-
 		fi
-	done<fname
+	done < $fname
 else
 	echo "$fname:No such file"
 fi
