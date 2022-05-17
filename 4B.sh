3ae3fe9ef11c75854624604f6eecc6c78904f335
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-05-17 14:29:59 +0530

    Updated

--- 4B.sh
+++ 4B.sh
@@ -1,22 +1,20 @@
 if [ $# -ge 1 ]
-then 
-if [ $# -eq 2 ]
-then
-
+then	
+	if [ $# -eq 2 ]
+	then
+		chd=$2
+	else
+		chd=`cd .`
+	fi
+	file=`find $chd -iname $1`
+	if [ `echo $file | wc -c` -gt 1 ]
+	then 
+		number=`ls -inum $file | grep -o ^[0-9]*`
+		cd $chd
+		find $chd -inum $number
+	else
+		echo No such file
+	fi
 else
- 
- fi
- file =`find ~-inum $1`
- if [ `echo $file | wc -c` -gt 1 ]
- then
- number=`ls -inum $file | grep -0 ^[0-9]`
-  
-  find . -inum $number
-  else
-  echo "No such file"
-  fi
-  else
-  echo ". filename"
-  fi
- 
-
+	echo Provide filename
+fi
