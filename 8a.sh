d64f87567f52c73acd9b4029d5623d0fc4948a15
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-05-26 22:33:36 +0530

    updated

--- 8a.sh
+++ 8a.sh
@@ -1,29 +1,32 @@
 if [ $# -eq 1 ]
 then 
-user=`who | grep -wc $1 `
-if [ $? -eq 0 ]
-then
-if ["$user"=`whoami` ]
-then
-currentHour=`date+ %H`
-currentMinute=`date+ m`
+	user=`who | grep -woi "$1"`
+	if [ $? -eq 0 ]
+	then
+	if [ "$user" = `whoami` ]
+	then 
+currentHour=`date +%H`
+currentMinute=`date +%M`
 currentUser=`whoami`
-set --`who | grep $currentUser`
-set --`$4 | tr ":" " "`
+set -- `who | grep $currentUser`
+set -- `echo $4 | tr ":" " "`
 loginHour=$1
 loginMinute=$2
-hours=`expr $currentHour-$loginHour`
-minutes=`expr $currentMinute-$loginMinute`
+hours=`expr $currentHour - $loginHour`
+minutes=`expr $currentMinute - $loginMinute`
 if [ $minutes -lt 0 ]
 then
-hours=`expr 60-$loginMinute`
-minutes=`expr $loginMinute-$currentMinute`
+hours=`expr $hour - 1 `
+loginMinute=`expr 60 - $loginMinute`
+minutes=`expr $loginMinute + $currentMinute`
 fi
-echo "The user `whoami` working time is $hour hours $minutes minutes"
+echo The user `whoami` working time is $hours hours $minutes minutes
 else
-echo "user not loggedin"
+echo user not logged in
 fi
 else
-echo "user doesn't exist"
-fi
+echo user does not exist
+fi 
+else
+echo enter parameter
 fi
