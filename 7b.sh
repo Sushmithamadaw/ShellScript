92ca119013094b585d86a9cd191db42f32b90fba
Sushmithamadaw <98949535+Sushmithamadaw@users.noreply.github.com>
2022-05-26 22:33:08 +0530

    updated

--- 7b.sh
+++ 7b.sh
@@ -1,27 +1,27 @@
 if [ $# -ne 0 ]
 then
-	if [ $# -lt 2 ]
-	then
-		echo Enter files to match
-	else
-		if [ -e $1 ]	
-		then
-			for fn in $*
-			do
-				if [ $fn = $1 ]
-				then
-					continue
-				fi
-				for w in `cat $1`
-				do
-					lc=`grep -wio "$w" $fn | wc -l`
-					echo $w is $lc times in $fn
-				done
-			done
-		else
-			echo Enter pattern file
-		fi
-	fi
+if [ $# -lt 2 ]
+then
+echo enter files to match
 else
-	echo Enter parameter
+if [ -e $1 ]
+then
+for fn in $*
+do
+ if [ $fn = $1 ]
+ then
+  continue
+  fi
+    for p in `cat $1`
+ do
+   l=`grep -wio "$p" $fn | wc -l`
+  echo $p is  $l times in $fn
+ done
+ done
+else
+  echo "Enter pattern"
+fi
+fi
+else
+echo  enter parameter
 fi
